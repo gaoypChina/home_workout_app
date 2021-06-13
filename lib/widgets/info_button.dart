5210f784dfb10135078580e808bdcfb551497f19
Akash Lilhare <akashlilhare14@gmail.com>
2021-06-13 23:19:52 +0530

    info button added

---
+++ lib/widgets/info_button.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfoButton extends StatelessWidget {
+  final IconData icon;
+  final Function onPress;
+  final String tooltip;
+
+  InfoButton({this.icon, this.onPress, this.tooltip});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 5.0,right: 8,bottom: 5),
+      child: Container(
+        height: 35,
+        width: 35,
+        child: FloatingActionButton(
+          backgroundColor: Colors.blue.shade200,
+          onPressed: onPress,
+          child: Icon(icon),
+          tooltip: tooltip,
+        ),
+      ),
+    );
+  }
+}
