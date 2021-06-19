3e243ea4dc93a761f48984e785c05f6ad170a630
Akash Lilhare <akashlilhare14@gmail.com>
2021-06-20 00:21:03 +0530

    rest day page added

---
+++ lib/pages/main/explore_page/rest_day_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RestDayPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Container(child: Text("to be implemented...."),));
+  }
+}
