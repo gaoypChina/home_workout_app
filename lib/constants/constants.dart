5dcc437ca8fc2bae3f24c1e6e062cdabce1c9404
Akash Lilhare <akashlilhare14@gmail.com>
2021-06-23 11:45:12 +0530

    constant file added

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../main.dart';
+
 class Constants{
   static Color primaryColor = Colors.blue;
   static Color secondaryTextColor = Colors.grey;
 
-  static Color  kBackgroundColor = Color(0xFFF8F8F8);
-  static  Color  kActiveIconColor = Colors.white;
-//Color(0xFFE68342);
-  static  Color  kTextColor = Color(0xFF222B45);
-  static Color  kBlueLightColor = Color(0xFFC7B8F5);
-  static  Color kBlueColor = Color(0xFF817DC0);
-  static Color kShadowColor = Color(0xFFE6E6E6);
-  static  Color purple = Color(0xff5c4db1);
-  static Color pink = Color(0xffdc4f89);
-  static Color svgColor = Color(0xff6369FF);
+
+
+
+
+  Color titleBgColor = Color(0xffF6F6F6);
+  Color titleColor = Color(0xffA9A9A9);
+  Color tileColor =Color(0xffF2F2F2);
+  Color leadingIconColor = Color(0xff969696);
+
+  var textStyle =textTheme.subtitle1.copyWith(wordSpacing:4,fontWeight: FontWeight.w700,fontSize: 18,color: Color(0xffA9A9A9));
+  var contentTextStyle = textTheme.subtitle2.copyWith(fontSize: 15);
+  Icon trailingIcon = Icon(Icons.arrow_forward_ios,color: Colors.black,size: 16,);
+
 }
